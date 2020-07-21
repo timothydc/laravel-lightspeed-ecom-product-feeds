498296e7c2e4091f259998cd1b33c2f86adc1ede
timothy <timothy.decort@polaris-dc.com>
2020-07-21 13:19:29 +0200

    wip

---
+++ tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TimothyDC\LightspeedEcomProductFeed\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function test_it_can_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
